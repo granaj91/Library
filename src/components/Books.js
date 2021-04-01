19b570ee7d059b523e63c3ba18dfac0811ca248f
granaj91 <32178706+granaj91@users.noreply.github.com>
2021-04-01 11:36:43 -0400

    Added div in Books and styled booklist

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,9 +2,9 @@ import Book from './Book'
 
 const Books = ({ books }) => {
     return (
-        <>
+        <div className="books">
             { books.map((book) => (<Book book={book}/>))}
-        </>
+        </div>
     )
 }
 
