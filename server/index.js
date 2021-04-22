47a6666acb024143de6b7e6a66605b569cf9ae3f
granaj91 <32178706+granaj91@users.noreply.github.com>
2021-04-22 13:18:36 -0400

    Moved package.json and dockerfile to server folder

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-const connectDB = require('../config/db');
+const connectDB = require('./config/db');
 
 dotenv.config({path: './config/.env'});
 
